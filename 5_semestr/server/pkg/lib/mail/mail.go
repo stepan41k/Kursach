e7e4a5546b6b7a51b43757d3fcee35b612f4812d
stepan41k <russia.zont@gmail.com>
2026-01-10 21:52:57 +0300

    feat: create or move auth, backup, mail, password logic to 'lib'

---
+++ 5_semestr/server/pkg/lib/mail/mail.go
@@ -0,0 +1,91 @@
+package mail
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+	"os"
+	"time"
+)
+
+func SendLoginEmail(toEmail string, userName string, ip string) {
+	host := os.Getenv("SMTP_HOST")
+	port := os.Getenv("SMTP_PORT")
+	user := os.Getenv("SMTP_USER") // Чистый email
+	pass := os.Getenv("SMTP_PASS")
+	from := os.Getenv("SMTP_FROM") // Красивое имя
+
+	if host == "" || toEmail == "" {
+		return
+	}
+
+	// Заголовки
+	headers := fmt.Sprintf("From: %s\r\n", from) +
+		fmt.Sprintf("To: %s\r\n", toEmail) +
+		"Subject: Оповещение о входе в RoseBank\r\n" +
+		"MIME-version: 1.0;\r\n" +
+		"Content-Type: text/plain; charset=\"UTF-8\";\r\n\r\n"
+
+	body := fmt.Sprintf("Здравствуйте, %s!\n\nБыл выполнен вход в ваш аккаунт.\nВремя: %s\nIP-адрес: %s\n\nЕсли это были не вы, срочно обратитесь к администратору.",
+		userName, time.Now().Format("02.01.2006 15:04"), ip)
+
+	msg := []byte(headers + body)
+
+	auth := smtp.PlainAuth("", user, pass, host)
+	addr := fmt.Sprintf("%s:%s", host, port)
+	
+	// ИСПРАВЛЕНИЕ ЗДЕСЬ: используем 'user' вместо 'from'
+	err := smtp.SendMail(addr, auth, user, []string{toEmail}, msg)
+
+	if err != nil {
+		log.Printf("Failed to send login email to %s: %v", toEmail, err)
+	} else {
+		log.Printf("Login notification sent to %s", toEmail)
+	}
+}
+
+
+func SendClientWelcomeEmail(toEmail string, fullName string, login string, password string) {
+	host := os.Getenv("SMTP_HOST")
+	port := os.Getenv("SMTP_PORT")
+	user := os.Getenv("SMTP_USER") // Чистый email (для авторизации и конверта)
+	pass := os.Getenv("SMTP_PASS")
+	from := os.Getenv("SMTP_FROM") // Красивое имя (для заголовка письма)
+
+	if host == "" || toEmail == "" {
+		log.Println("SMTP not configured or email empty")
+		return
+	}
+
+	// Заголовки письма (Headers)
+	// Важно: заголовки отделяются от тела пустой строкой (\r\n)
+	headers := fmt.Sprintf("From: %s\r\n", from) +
+		fmt.Sprintf("To: %s\r\n", toEmail) +
+		"Subject: Добро пожаловать в RoseBank!\r\n" +
+		"MIME-version: 1.0;\r\n" +
+		"Content-Type: text/plain; charset=\"UTF-8\";\r\n\r\n"
+
+	body := fmt.Sprintf(
+		"Здравствуйте, %s!\n\n"+
+			"Вы были успешно зарегистрированы в системе RoseBank.\n"+
+			"Для входа в Личный кабинет клиента используйте следующие данные:\n\n"+
+			"Логин: %s\n"+
+			"Пароль: %s\n\n"+
+			"Пожалуйста, никому не сообщайте свой пароль.",
+		fullName, login, password,
+	)
+
+	msg := []byte(headers + body)
+	addr := fmt.Sprintf("%s:%s", host, port)
+	auth := smtp.PlainAuth("", user, pass, host)
+
+	// ИСПРАВЛЕНИЕ ЗДЕСЬ:
+	// 3-й аргумент - это 'user' (чистый email), а не 'from'
+	err := smtp.SendMail(addr, auth, user, []string{toEmail}, msg)
+	
+	if err != nil {
+		log.Printf("Failed to send welcome email to %s: %v", toEmail, err)
+	} else {
+		log.Printf("Welcome email sent to %s", toEmail)
+	}
+}
---
+++ 5_semestr/server/pkg/lib/password/password.go
@@ -0,0 +1,13 @@
+package password
+
+import "math/rand"
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateRandomPassword(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
