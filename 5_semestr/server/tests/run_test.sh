246893434ed1a32050a7f392ead4f7a42a5cef00
stepan41k <russia.zont@gmail.com>
2026-01-16 05:09:06 +0300

    fix: change run_tests.sh file

--- 5_semestr/server/tests/run_test.sh
+++ 5_semestr/server/tests/run_test.sh
@@ -6,22 +6,21 @@ DB_NAME="mydb"
 TEST_FILE="./tests.sql"
 
 if [ ! "$(docker ps -q -f name=$DB_CONTAINER)" ]; then
-    echo "ERROR: Контейнер '$DB_CONTAINER' не запущен"
+    echo "ERROR: Container '$DB_CONTAINER' not running"
     exit 1
 fi
 
 if [ ! -f "$TEST_FILE" ]; then
-    echo "ERROR: Файл '$TEST_FILE' не найден"
+    echo "ERROR: File '$TEST_FILE' not found"
     exit 1
 fi
 
-echo "Запуск тестов из $TEST_FILE..."
 
 cat "$TEST_FILE" | docker exec -i $DB_CONTAINER psql -U $DB_USER -d $DB_NAME -v ON_ERROR_STOP=1
 
 if [ $? -eq 0 ]; then
     echo "All tests passed successfully"
 else
-    echo "Произошла ошибка при выполнении SQL-скрипта."
+    echo "Error while try to execute sql-script"
     exit 1
 fi
